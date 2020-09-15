944bc00e1ad2ccdbb418dc4f5beebc70a57fad93
Samwel Mwaura <smmuiruri@gmail.com>
2020-09-15 03:56:06 -0700

    install react-testing-library and jest-dom

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './components/App';
 
+// use describe, it pattern
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render();
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
